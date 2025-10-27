bf292951ff67cb7c07d340829f66fa4ebfecc145
DESKTOP-7MUDD6R\Thanh <Thanh.CEAA.K58.HUST@gmail.com>
2025-10-28 00:55:04 +0700

    Add GradTTS with Speaker Embedding as input

--- model/__init__.py
+++ model/__init__.py
@@ -6,4 +6,4 @@
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 # MIT License for more details.
 
-from .tts import GradTTS, VarianceAdaptorGradTTS
+from .tts import GradTTS, VarianceAdaptorGradTTS, GradTTSWithSpeakerEmbedding
