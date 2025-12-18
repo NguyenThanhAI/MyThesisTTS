b9c9d775496e7ff3c2052420267329aa2a54eeff
DESKTOP-7MUDD6R\Thanh <Thanh.CEAA.K58.HUST@gmail.com>
2025-12-18 23:10:10 +0700

    Add metrics list to choose

--- compute_all_metrics.py
+++ compute_all_metrics.py
@@ -11,12 +11,14 @@ if __name__ == "__main__":
     parser.add_argument("--model_name", type=str, required=False, default="grad_tts_multi_speaker_LJSpeech_use_saln_steps_1167000", help="Name of the model.")
     parser.add_argument("--dataset_name", type=str, required=False, default="LJSpeech", help="Name of the dataset.")
     parser.add_argument("--reference_data_dir", type=str, required=False, default=r"D:\TTS_Raw_Dataset", help="Path to the reference data directory.")
+    parser.add_argument("--metrics_list", type=str, nargs='+', default=["wer_un_comma"], help="List of metrics to compute.")
     args = parser.parse_args()
 
     synthesized_data_dir = args.synthesized_data_dir
     model_name = args.model_name
     dataset_name = args.dataset_name
     reference_data_dir = args.reference_data_dir
+    metrics_list = args.metrics_list
 
     metrics_cal = MetricCalculator(
         synthetized_data_dir=synthesized_data_dir,
@@ -25,7 +27,12 @@ if __name__ == "__main__":
         reference_data_dir=reference_data_dir
     )
 
-    metrics = metrics_cal.get_all_metrics()
+    if metrics_list is not None:
+        metrics = metrics_cal.get_metrics_by_list(metric_list=metrics_list)
+    else:
+        metrics = metrics_cal.get_all_metrics()
+
+    print(f"Computed Metrics: {json.dumps(metrics, indent=4)}")
 
     save_dir = "metrics_results"
 
